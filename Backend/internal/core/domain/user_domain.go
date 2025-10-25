169b4a16f4ffa2b88f32ee0e189abf4fe121a5d6
Nudhana Sarutipaisan <nu37317@gmail.com>
2025-10-25 22:44:28 +0700

    first commit

--- Backend/internal/core/domain/user_domain.go
+++ Backend/internal/core/domain/user_domain.go
@@ -3,8 +3,8 @@ package domain
 type UserRegisterRequest struct {
 	Email     string `json:"email" validate:"required,email"`
 	Password  string `json:"password" validate:"required,min=8"`
-	Firstname string `json:"firstname" validate:"required"`
-	Lastname  string `json:"lastname" validate:"required"`
+	Firstname string `json:"firstname" validate:"required,min=1"`
+	Lastname  string `json:"lastname" validate:"required,min=1"`
 }
 
 type UserLoginRequest struct {
